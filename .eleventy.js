554979e4ab97a91e5d0e9bd0d9fa19d8d3bace7a
Hafid HADDAOUI <hahaddaoui@gmail.com>
2022-08-30 14:59:26 +0200

    first init

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+
+}
