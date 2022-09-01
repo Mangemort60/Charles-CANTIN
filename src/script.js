e60461974b19ac16ab1edc0e95d42c8d2395f108
Hafid HADDAOUI <hahaddaoui@gmail.com>
2022-09-01 20:50:53 +0200

    edit navbar social

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const social = document.getElementsByClassName('navbar-social')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  navbarLinks.classList.toggle('active');
+  social.classList.toggle('active')
 })
