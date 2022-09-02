214ae41fbe0f4779262a66a6ed51f1f7aeefcd84
Hafid HADDAOUI <hahaddaoui@gmail.com>
2022-09-02 20:39:53 +0200

    edit galerie

--- src/script.js
+++ src/script.js
@@ -7,3 +7,37 @@ toggleButton.addEventListener('click', () => {
   social.classList.toggle('active')
 })
 
+
+/*FILTER PHOTOS*/
+
+const filterContainer = document.querySelector("category");
+const galleryItems = document.querySelectorAll("filter-item");
+
+filterContainer.addEventListener("click", (event) =>{
+  if(event.target.classList.contains("filter-item")){
+
+    // deactivate existing active 'filter-item'
+   filterContainer.querySelector(".active").classList.remove("active");
+
+    // activate new 'filter-item'
+   event.target.classList.add("active");
+
+   const filterValue = event.target.getAttribute("data-filter");
+
+   galleryItems.forEach((item) =>{
+
+      if(item.classList.contains(filterValue) || filterValue === 'all'){
+       item.classList.remove("hide");
+        item.classList.add("show");
+      }
+
+      else{
+       item.classList.remove("show");
+       item.classList.add("hide");
+      }
+
+    });
+  }
+});
+
+
