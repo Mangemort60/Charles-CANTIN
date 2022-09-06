bb45eee708c855e8d4d336ea4dee268a3fc3fe30
Hafid HADDAOUI <hahaddaoui@gmail.com>
2022-09-06 12:29:32 +0200

    finalizing

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+/********** MENU DEROULANT ***********/
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const social = document.getElementsByClassName('navbar-social')[0]
@@ -8,40 +10,40 @@ toggleButton.addEventListener('click', () => {
 })
 
 
-/*FILTER PHOTOS*/
+/**********FILTRE GALLERIE PAR CATEGORIE*************/
 
+// recupère les buttons de categorie dans une nodelist
 const buttonCategorie = document.querySelectorAll(".items span.item");
-// creer un array a partir d'une nodelist
+// crée un Array à partir de la nodelist buttonCategorie 
 const arrButtonCategorie = Array.from(buttonCategorie)
 
-// PHOTOS
-// recupère les photos dans une nodelist(array)
+// recupère les photos dans une nodelist 
 const photoCategorie = document.querySelectorAll(".photos img");
-
+// crée un Array à partir de la nodelist arrButtonCategorie
 const arrPhotoCategorie = Array.from(photoCategorie);
 
-// const attributClass = arrPhotoCategorie.map( photo => {
-  //   console.log( photo.getAttribute("class"))
-  // })
 
-  
+// parcours l'Array arrButtonCategorie  
 const mapButton = arrButtonCategorie.map( element => {
-  // select all button ID
+  // pour chaque élément(button) on créer un event click 
   element.addEventListener("click", () => {
+    // on parcours l'Array arrPhotoCategorie
     arrPhotoCategorie.map( photo => {
-
+      // pour chaque élement(photo) on récupère le contenu en string du bouton
       const value = element.textContent;
-      console.log(value);
 
+      // puis on vérifie : 
+      // si l'attribut class de l'element photo contient la string du bouton
+      // si c'est le cas on affiche l'element photo      
       if(photo.className.includes(value.toLocaleLowerCase())){
         photo.style.display = 'block'
-        
-
-        console.log(true);
-        }   else if (value == 'Tout') {
+        }   
+        // si la string du bouton = 'tout' on affiche toute les élements photo 
+        else if (value == 'Tout') {
             arrPhotoCategorie.forEach( photo => 
               photo.style.display = "block")
         }     
+        // sinon on affiche rien
         else {
         photo.style.display = 'none'
       }
